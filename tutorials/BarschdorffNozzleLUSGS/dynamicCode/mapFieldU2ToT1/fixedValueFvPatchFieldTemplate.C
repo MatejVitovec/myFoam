9a8447b7d90ac6f62ed4ebdfb6b2b5c630897f98
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2025-08-27 13:25:42 +0200

    pridani wetSteamLusgsFoam, prvni test nutno pridat implicitni dragSourceTerm

---
+++ tutorials/BarschdorffNozzleLUSGS/dynamicCode/mapFieldU2ToT1/fixedValueFvPatchFieldTemplate.C
@@ -0,0 +1,214 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2019-2021 OpenCFD Ltd.
+    Copyright (C) YEAR AUTHOR, AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fixedValueFvPatchFieldTemplate.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "unitConversion.H"
+#include "PatchFunction1.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+// dynamicCode:
+// SHA1 = bf766773243118fdfeb54c6f9d87d057e8e10002
+//
+// unique function name that can be checked if the correct library version
+// has been loaded
+extern "C" void mapFieldU2ToT1_bf766773243118fdfeb54c6f9d87d057e8e10002(bool load)
+{
+    if (load)
+    {
+        // Code that can be explicitly executed after loading
+    }
+    else
+    {
+        // Code that can be explicitly executed before unloading
+    }
+}
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+makeRemovablePatchTypeField
+(
+    fvPatchVectorField,
+    mapFieldU2ToT1FixedValueFvPatchVectorField
+);
+
+} // End namespace Foam
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+mapFieldU2ToT1FixedValueFvPatchVectorField
+(
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF
+)
+:
+    parent_bctype(p, iF)
+{
+    if (false)
+    {
+        printMessage("Construct mapFieldU2ToT1 : patch/DimensionedField");
+    }
+}
+
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+mapFieldU2ToT1FixedValueFvPatchVectorField
+(
+    const mapFieldU2ToT1FixedValueFvPatchVectorField& rhs,
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    parent_bctype(rhs, p, iF, mapper)
+{
+    if (false)
+    {
+        printMessage("Construct mapFieldU2ToT1 : patch/DimensionedField/mapper");
+    }
+}
+
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+mapFieldU2ToT1FixedValueFvPatchVectorField
+(
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    parent_bctype(p, iF, dict)
+{
+    if (false)
+    {
+        printMessage("Construct mapFieldU2ToT1 : patch/dictionary");
+    }
+}
+
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+mapFieldU2ToT1FixedValueFvPatchVectorField
+(
+    const mapFieldU2ToT1FixedValueFvPatchVectorField& rhs
+)
+:
+    parent_bctype(rhs),
+    dictionaryContent(rhs)
+{
+    if (false)
+    {
+        printMessage("Copy construct mapFieldU2ToT1");
+    }
+}
+
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+mapFieldU2ToT1FixedValueFvPatchVectorField
+(
+    const mapFieldU2ToT1FixedValueFvPatchVectorField& rhs,
+    const DimensionedField<vector, volMesh>& iF
+)
+:
+    parent_bctype(rhs, iF)
+{
+    if (false)
+    {
+        printMessage("Construct mapFieldU2ToT1 : copy/DimensionedField");
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::
+~mapFieldU2ToT1FixedValueFvPatchVectorField()
+{
+    if (false)
+    {
+        printMessage("Destroy mapFieldU2ToT1");
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void
+Foam::
+mapFieldU2ToT1FixedValueFvPatchVectorField::updateCoeffs()
+{
+    if (this->updated())
+    {
+        return;
+    }
+
+    if (false)
+    {
+        printMessage("updateCoeffs mapFieldU2ToT1");
+    }
+
+//{{{ begin code
+    #line 32 "/home/matej/myFoam/tutorials/BarschdorffNozzleLUSGS/0/U.2/boundaryField/inlet"
+const volVectorField& sourceField = db().lookupObject<volVectorField>("U.1");
+            operator== (sourceField.boundaryField()[patch().index()]);
+//}}} end code
+
+    this->parent_bctype::updateCoeffs();
+}
+
+
+// ************************************************************************* //
+
---
+++ wetSteamLusgsFoam/wetSteamLusgsFoam.C
@@ -0,0 +1,257 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2021 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    dbFoam
+
+Group
+    grpCompressibleSolvers
+
+Description
+    Density-based compressible flow solver
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+
+#include "twoFluid.H"
+#include "twoFluidConvectiveFlux.H"
+#include "twoFluidFvc.H"
+#include "dragModel.H"
+#include "saturationCurve.H"
+#include "condensationModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Density-based compressible flow solver based on"
+        " Riemann problem flux calculation"
+    );
+
+    #define NO_CONTROL
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+    #include "createTimeControls.H"
+    #include "readLUSGSControls.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    // Courant numbers used to adjust the time-step
+    scalar CoNum = 0.0;
+    scalar meanCoNum = 0.0;
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+
+        amaxSf = fluid.amaxSf();
+        #include "courantNo.H"
+        #include "setDeltaT.H"
+        ++runTime;
+
+        Info << "Iter = " << runTime.timeIndex() << endl;
+        Info << "Time = " << runTime.timeName() << endl;
+
+        dimensionedScalar dt = runTime.deltaT();
+
+        volVectorField dragSource
+        (
+            "dragS",
+            drag.K(condensation.dropletDiameter())*(U1 - U2)
+        );
+
+        scalar initialRezAlphaRho1 = 0, initialRezAlphaRho2 = 0, initialRezAlphaRhoU1 = 0, initialRezAlphaRhoU2 = 0, initialRezEpsilon1 = 0, initialRezEpsilon2 = 0;
+
+
+        for (int intIter = 0; intIter < lusgsIntIters; intIter++)
+        {
+            twoFluidFlux.computeFlux();
+
+            /////// NOVE
+            volVectorField Uint = (alpha1*rho1*U1 + alpha2*rho2*U2)/(alpha1*rho1 + alpha2*rho2);
+
+            volScalarField Ts = condensation.saturation().Ts(p);
+            volScalarField Hvint = condensation.saturation().hsv(Ts) + (Uint & U1) - 0.5*magSqr(U1);
+            volScalarField Hlint = condensation.saturation().hsl(Ts) + (Uint & U2) - 0.5*magSqr(U2);
+
+            dragSource = drag.K(condensation.dropletDiameter())*(U1 - U2);
+
+            volScalarField condensationMassSource = condensation.condensationRateMassSource();
+            volVectorField condensationMomentumSource = Uint*condensation.condensationRateMassSource();
+            volScalarField condensationEnergyVaporSource = condensation.condensationRateMassSource()*(Hvint - condensation.L());
+            volScalarField condensationEnergyLiquidSource = condensation.condensationRateMassSource()*Hlint;
+            ////// END NOVE
+
+            volScalarField dAlphaRho1(-dt*(
+                fvc::ddt(conservative.alphaRho1()) + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoFlux1_pos(), twoFluidFlux.alphaRhoFlux1_neg()) + condensationMassSource
+            ));
+
+            volScalarField dAlphaRho2(-dt*(
+                fvc::ddt(conservative.alphaRho2()) + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoFlux2_pos(), twoFluidFlux.alphaRhoFlux2_neg()) - condensationMassSource
+            ));
+
+            volVectorField dAlphaRhoU1(-dt*(
+                fvc::ddt(conservative.alphaRhoU1())
+                + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoUFlux1_pos(), twoFluidFlux.alphaRhoUFlux1_neg())
+                - fluid.pInt()*TwoFluidFoam::fvc::div(twoFluidFlux.alpha1_pos()*mesh.Sf(), twoFluidFlux.alpha1_neg()*mesh.Sf())
+                //+ drag.K(d)*(fluid.U1() - fluid.U2())
+                + dragSource + condensationMomentumSource
+            ));
+
+            volVectorField dAlphaRhoU2(-dt*(
+                fvc::ddt(conservative.alphaRhoU2())
+                + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoUFlux2_pos(), twoFluidFlux.alphaRhoUFlux2_neg())
+                - fluid.pInt()*TwoFluidFoam::fvc::div(twoFluidFlux.alpha2_pos()*mesh.Sf(), twoFluidFlux.alpha2_neg()*mesh.Sf())
+                //+ drag.K(d)*(fluid.U2() - fluid.U1())
+                - dragSource - condensationMomentumSource
+            ));
+
+            volScalarField dEpsilon1(-dt*(
+                fvc::ddt(conservative.epsilon1())
+                + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoEFlux1_pos(), twoFluidFlux.alphaRhoEFlux1_neg())
+                //+ (dragTerm & U1)
+                + (dragSource & Uint) + condensationEnergyVaporSource
+            ));
+
+            volScalarField dEpsilon2(-dt*(
+                fvc::ddt(conservative.epsilon2())
+                + TwoFluidFoam::fvc::div(twoFluidFlux.alphaRhoEFlux2_pos(), twoFluidFlux.alphaRhoEFlux2_neg())
+                //- (dragTerm & U2)
+                - (dragSource  & Uint) - condensationEnergyLiquidSource
+            ));
+
+            scalar rezAlphaRho1  = fvc::domainIntegrate(mag(dAlphaRho1) /dt).value();
+            scalar rezAlphaRho2  = fvc::domainIntegrate(mag(dAlphaRho2) /dt).value();
+            scalar rezAlphaRhoU1 = fvc::domainIntegrate(mag(dAlphaRhoU1)/dt).value();
+            scalar rezAlphaRhoU2 = fvc::domainIntegrate(mag(dAlphaRhoU2)/dt).value();
+            scalar rezEpsilon1   = fvc::domainIntegrate(mag(dEpsilon1)  /dt).value();
+            scalar rezEpsilon2   = fvc::domainIntegrate(mag(dEpsilon2)  /dt).value();
+
+            if (intIter == 0)
+            {
+                initialRezAlphaRho1  = rezAlphaRho1;
+                initialRezAlphaRho2  = rezAlphaRho2;
+                initialRezAlphaRhoU1 = rezAlphaRhoU1;
+                initialRezAlphaRhoU2 = rezAlphaRhoU2;
+                initialRezEpsilon1   = rezEpsilon1;
+                initialRezEpsilon2   = rezEpsilon2;
+            }
+
+#           include "lusgsSweep.H"
+
+            conservative.alphaRho1()  += dAlphaRho1;
+            conservative.alphaRho2()  += dAlphaRho2;
+            conservative.alphaRhoU1() += dAlphaRhoU1;
+            conservative.alphaRhoU2() += dAlphaRhoU2;
+            conservative.epsilon1()   += dEpsilon1;
+            conservative.epsilon2()   += dEpsilon2;
+
+
+            fluid.correct();
+            fluid.blendVanishingFluid();
+            //fluid.bound();
+            fluid.correctBoundaryCondition();
+
+            if (runTime.timeIndex() > 6840)
+            {
+                runTime.writeNow();
+                runTime.write();
+            }
+
+            fluid.correctThermo();
+            fluid.correctInterfacialPressure();
+            fluid.correctConservative();
+
+            scalar finalRezAlphaRho1  = fvc::domainIntegrate(mag(dAlphaRho1) /dt).value();
+            scalar finalRezAlphaRho2  = fvc::domainIntegrate(mag(dAlphaRho2) /dt).value();
+            scalar finalRezAlphaRhoU1 = fvc::domainIntegrate(mag(dAlphaRhoU1)/dt).value();
+            scalar finalRezAlphaRhoU2 = fvc::domainIntegrate(mag(dAlphaRhoU2)/dt).value();
+            scalar finalRezEpsilon1   = fvc::domainIntegrate(mag(dEpsilon1)  /dt).value();
+            scalar finalRezEpsilon2   = fvc::domainIntegrate(mag(dEpsilon2)  /dt).value();
+
+            Info << "LUSGS:  Solving for alphaRho1,          " << "Initial residual = " << rezAlphaRho1  << ", " << "Final residual = " << finalRezAlphaRho1  << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alphaRho2,          " << "Initial residual = " << rezAlphaRho2  << ", " << "Final residual = " << finalRezAlphaRho2  << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alphaRhoU1,         " << "Initial residual = " << rezAlphaRhoU1 << ", " << "Final residual = " << finalRezAlphaRhoU1 << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alphaRhoU2,         " << "Initial residual = " << rezAlphaRhoU2 << ", " << "Final residual = " << finalRezAlphaRhoU2 << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alpha1(rhoE1+pInt), " << "Initial residual = " << rezEpsilon1   << ", " << "Final residual = " << finalRezEpsilon1   << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alpha2(rhoE2+pInt), " << "Initial residual = " << rezEpsilon2   << ", " << "Final residual = " << finalRezEpsilon2   << ", No Iterations 1" << nl;
+
+            bool lastIteration = ((intIter + 1) == lusgsIntIters);
+
+            lastIteration = lastIteration || ( 
+                (finalRezAlphaRho1  < lusgsRelTol * initialRezAlphaRho1) && 
+                (finalRezAlphaRho2  < lusgsRelTol * initialRezAlphaRho2) && 
+                (finalRezAlphaRhoU1 < lusgsRelTol * initialRezAlphaRhoU1) && 
+                (finalRezAlphaRhoU2 < lusgsRelTol * initialRezAlphaRhoU2) && 
+                (finalRezEpsilon1   < lusgsRelTol * initialRezEpsilon1) && 
+                (finalRezEpsilon2   < lusgsRelTol * initialRezEpsilon2) );
+
+            lastIteration = lastIteration || ( 
+                (finalRezAlphaRho1  < lusgsTolerance) && 
+                (finalRezAlphaRho2  < lusgsTolerance) && 
+                (finalRezAlphaRhoU1 < lusgsTolerance) && 
+                (finalRezAlphaRhoU2 < lusgsTolerance) && 
+                (finalRezEpsilon1   < lusgsTolerance) && 
+                (finalRezEpsilon2   < lusgsTolerance) );
+
+            if (lastIteration) {
+                Info << "LUSGS: converged in " << intIter + 1 << " iterations." << nl << nl;
+                break;
+            }
+        }
+
+        rho = fluid.rho();
+        rhoMPhi2 = linearInterpolate(rho*U2) & mesh.Sf();
+        condensation.correct();
+
+        rho1.ref() = thermo1.rho();
+        rho2.ref() = thermo2.rho();
+
+        runTime.write();
+
+        //runTime.printExecutionTime(Info);
+
+        //std::cin.ignore();
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
