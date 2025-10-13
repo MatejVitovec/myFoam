fba9a68e8d4b575a698dba05fb9297bd0dd5f1d8
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2025-10-13 23:32:58 +0200

    male upravy TFLUSGS

--- applications/solvers/multiphase/lusgsTwoFluidFoam/lusgsTwoFluidFoam.C
+++ applications/solvers/multiphase/lusgsTwoFluidFoam/lusgsTwoFluidFoam.C
@@ -176,12 +176,12 @@ int main(int argc, char *argv[])
             scalar finalRezT1    = fvc::domainIntegrate(mag(dT1)   /dt).value();
             scalar finalRezT2    = fvc::domainIntegrate(mag(dT2)   /dt).value();
 
-            Info << "LUSGS:  Solving for p,        " << "Final residual = " << finalRezp     << ", No Iterations 1" << nl;
-            Info << "LUSGS:  Solving for alpha,    " << "Final residual = " << finalRezalpha << ", No Iterations 1" << nl;
-            Info << "LUSGS:  Solving for alphaU1,  " << "Final residual = " << finalRezU1    << ", No Iterations 1" << nl;
-            Info << "LUSGS:  Solving for alphaU2,  " << "Final residual = " << finalRezU2    << ", No Iterations 1" << nl;
-            Info << "LUSGS:  Solving for T1,       " << "Final residual = " << finalRezT1    << ", No Iterations 1" << nl;
-            Info << "LUSGS:  Solving for T2,       " << "Final residual = " << finalRezT2    << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for p,     " << "Final residual = " << finalRezp     << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for alpha, " << "Final residual = " << finalRezalpha << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for U1,    " << "Final residual = " << finalRezU1    << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving forU2,     " << "Final residual = " << finalRezU2    << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for T1,    " << "Final residual = " << finalRezT1    << ", No Iterations 1" << nl;
+            Info << "LUSGS:  Solving for T2,    " << "Final residual = " << finalRezT2    << ", No Iterations 1" << nl;
 
             bool lastIteration = ((intIter + 1) == lusgsIntIters);
 
